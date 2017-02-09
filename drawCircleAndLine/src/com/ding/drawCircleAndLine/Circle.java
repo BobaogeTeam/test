6c69fbdfdab183785a475b9a65b3460886be2563
”sjw123456 <1175476869@qq.com>
2017-02-09 11:28:20 +0800

    Signed-off-by: ”sjw123456 <1175476869@qq.com>

--- drawCircleAndLine/src/com/ding/drawCircleAndLine/Circle.java
+++ drawCircleAndLine/src/com/ding/drawCircleAndLine/Circle.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 /**
  * Created by dingli on 2015-7-1.
- * 画圆点111
+ * 画圆点111222
  */
 public class Circle extends View {
     private int color=getResources().getColor(R.color.red_ff3939),radius=0;
